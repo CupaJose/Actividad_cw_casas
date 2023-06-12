51e779b35a6c99243bdf45ea98f8afe0ee4cc560
Cupa José <cupa.sepulveda.jc@gmail.com>
2023-06-11 22:14:49 -0600

    Corrección de variables para la conexion

--- Dynamics/php/config.php
+++ Dynamics/php/config.php
@@ -1,12 +1,17 @@
 <?php
-    const DBHOST = "localhost";
-    const DBUSER = "root";
-    CONST DBPASS = "";
-    CONST DB = "casas_cw";
+    define("DBUSER","root");
+    define("DBHOST","localhost");
+    define("DBPASS","");
+    define("DB","casas_cw");
 
     function connect()
     {
         $conexion = mysqli_connect(DBHOST,DBUSER,DBPASS,DB);
+        if(!$conexion)
+        {
+            mysqli_connect_error();
+            echo "No se pudo";
+        }
         return $conexion;
     }
 ?>
